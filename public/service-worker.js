40eedcf375ce9387a2cbfc1843aba34d4fab982f
Madeline Jimenez <madelineijimenez@gmail.com>
2020-03-20 09:52:42 -0400

    Install code on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,3 +11,15 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// Install - when user installs service worker we take files and load them into the cache - we call call cache and open it up - wen it’s done, open cache and add all files to it, files to cache points back to array of sources!
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+});
+
